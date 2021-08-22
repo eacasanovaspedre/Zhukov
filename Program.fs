7ea5f2dd13a9c8b00387bc23ea4dc0d70598aab3
Enrique Casanovas <eacasanovaspedre@outlook.com>
2021-08-22 19:07:08 -0400

    updated tests in program

--- Program.fs
+++ Program.fs
@@ -50,14 +50,23 @@ let main argv =
                 (fun x -> Job.result ())
 
         let! consumerShutdownId =
-            Shutdown.register (Some "consumer1") (fun () -> MailboxProcessorStop.stop consumer1.Mailbox >>=. consumer1.Stopped) [] shutdown.Mailbox
+            Shutdown.register
+                (Some "consumer1")
+                (fun () ->
+                    MailboxProcessorStop.stop consumer1.Mailbox
+                    >>=. consumer1.Stopped)
+                []
+                shutdown.Mailbox
 
         let! poll1 = MailboxProcessorStop.sendAndAwaitReply consumer1.Mailbox (fun r -> Consumer.Action.Poll(5, 2, r))
 
         printfn "poll %A" poll1
 
-        let k1 = MessageKey(Flux.Text.stringToBytesUTF8 "k1")
-        let k2 = MessageKey(Flux.Text.stringToBytesUTF8 "k2")
+        let k1 =
+            MessageKey(Flux.Text.stringToBytesUTF8 "k1")
+
+        let k2 =
+            MessageKey(Flux.Text.stringToBytesUTF8 "k2")
 
         do!
             MailboxProcessorStop.send
@@ -73,20 +82,34 @@ let main argv =
                 consumer1.Mailbox
                 (Consumer.Action.AddQueue(
                     k2,
-                    { Queue = Flux.Collections.Queue.ofSeq ["k2m1"; "k2m2"; "k2m3"; "k2m4"; "k2m5"; "k2m6"; "k2m7"; "k2m8"; "k2m9"]
+                    { Queue =
+                          Flux.Collections.Queue.ofSeq [ "k2m1"
+                                                         "k2m2"
+                                                         "k2m3"
+                                                         "k2m4"
+                                                         "k2m5"
+                                                         "k2m6"
+                                                         "k2m7"
+                                                         "k2m8"
+                                                         "k2m9" ]
                       Offset = offset 1320 }
                 ))
 
         do!
             MailboxProcessorStop.send
                 consumer1.Mailbox
-                (Consumer.Action.NewMessage(MessageKey(Flux.Text.stringToBytesUTF8 "k1"), "message1"))
+                (Consumer.Action.NewMessage(
+                    MessageKey(Flux.Text.stringToBytesUTF8 "k1"),
+                    "message1",
+                    fun _ _ -> Job.unit ()
+                ))
 
         let! poll2 = MailboxProcessorStop.sendAndAwaitReply consumer1.Mailbox (fun r -> Consumer.Action.Poll(5, 2, r))
 
         printfn "poll %A" poll2
 
-        let! ack1 = MailboxProcessorStop.sendAndAwaitReply consumer1.Mailbox (fun r -> Consumer.Action.Ack(k2, offset 1322, r))
+        let! ack1 =
+            MailboxProcessorStop.sendAndAwaitReply consumer1.Mailbox (fun r -> Consumer.Action.Ack(k2, offset 1322, r))
 
         printfn "ack %A" ack1
 
